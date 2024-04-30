5cd57191a89e155c5470315373db853428e9ce4e
EggnogBaileys <malonemast@gmail.com>
2024-04-30 12:28:01 -0400

    Added placeholder oneleg and ball images

---
+++ scenes/gooper/state_machine.gd
@@ -0,0 +1,42 @@
+extends Node
+
+@export var initial_state : gooper_state
+
+var current_state : gooper_state
+var states : Dictionary = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child in get_children():
+		if child is gooper_state:
+			states[child.name.to_lower()] = child
+			child.changed_state.connect(on_state_changed)
+
+	if initial_state:
+		initial_state.Enter()
+		current_state = initial_state
+
+func _process(delta):
+	if current_state:
+		current_state.Update(delta)
+
+func _physics_process(delta):
+	if current_state:
+		current_state.Physics_Update(delta)
+
+func on_state_changed(state, new_state_name):
+	if state != current_state:
+		return 
+		
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		return 
+		
+	if current_state:
+		current_state.Exit()
+		
+	new_state.Enter()
+	
+	current_state = new_state
+	
+	
---
+++ scripts/gooper/gooper_state.gd
@@ -0,0 +1,24 @@
+extends Node
+class_name gooper_state
+
+signal changed_state
+
+
+func Enter():
+	pass
+	
+
+func Exit():
+	pass 
+	
+
+func Update(_delta: float):
+	pass 
+	
+	
+func Physics_Update(_delta: float):
+	pass
+
+
+
+
